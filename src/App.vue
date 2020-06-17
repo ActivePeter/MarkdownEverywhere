6f978b56f146690fba29d0ac2de633c3b793f021
ActivePeter <1020401660@qq.com>
2020-06-17 16:10:16 +0800

    treemenu first build

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,10 @@
     <div id="app">
       <transition name="fade">
       <div class="sidebar" v-show="showSideBar"
-       
       >
-      
+        <TreeMenu
+        :treeData="treeData"
+        />
       </div></transition>
       <div class="article">
         <mavon-editor
@@ -34,50 +35,32 @@
   import NavMenu from '@/components/content/navmenu/NavMenu'
   import { mavonEditor } from 'mavon-editor'
   import 'mavon-editor/dist/css/index.css'
+  import TreeMenu from '@/views/components/TreeMenu'
   export default {
     name: "App",
     components:{
       NavMenu,
-      mavonEditor
+      mavonEditor,
+      TreeMenu
     },
     data(){
       return{
         showSideBar:true,
-        data: [{
-          label: '一级 1',
-          children: [{
-            label: '二级 1-1',
-            children: [{
-              label: '三级 1-1-1'
-            }]
-          }]
-        }, {
-          label: '一级 2',
-          children: [{
-            label: '二级 2-1',
-            children: [{
-              label: '三级 2-1-1'
-            }]
-          }, {
-            label: '二级 2-2',
-            children: [{
-              label: '三级 2-2-1'
-            }]
-          }]
-        }, {
-          label: '一级 3',
-          children: [{
-            label: '二级 3-1',
-            children: [{
-              label: '三级 3-1-1'
-            }]
-          }, {
-            label: '二级 3-2',
-            children: [{
-              label: '三级 3-2-1'
-            }]
-          }]
-        }],
+        treeData: [
+          {
+            label: '一级 1',
+            children: [
+              {
+                label: '二级 1-1',
+                children: [
+                  {
+                    label: '三级 1-1-1'
+                  }
+                ]
+              }
+            ]
+          }
+        ],
       }
     },
     methods:{
@@ -98,13 +81,14 @@
     display: flex;
   }
   .sidebar{
-    
+    height: 100vh;
     background:green;
     width: 100px;
+    padding-top: 60px;
   }
   .article{
     padding-top: 60px;
-    min-height:100vh;
+    height:100vh;
     width: 100%;
   }
   .fade-enter-active, .fade-leave-active {
---
+++ src/views/components/TreeMenu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="all">
+    <div
+      :key="index"
+      v-for="item,index in treeData">
+    >
+      {{index}}
+    </div>
+  </div>
+  
+</template>
+
+<script>
+  import NavMenu from '@/components/content/navmenu/NavMenu'
+  export default {
+    name: "treeMenu",
+    components:{
+      
+    },
+    data(){
+      return{
+      }
+    },
+    methods:{
+    },
+    props:{
+      treeData:{//作用是请求的时候提供数据，还有整明为子集评论框
+        type:Array,
+        default() {
+          return [];
+        }
+      },
+    }
+  }
+</script>
+
+<style scoped>
+.all{
+  width: 200px;
+}
+</style>
